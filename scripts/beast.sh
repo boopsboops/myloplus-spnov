d47e5a6ba32c5ee6f0656a709dcbfb2384139867
rupert@it031740 <rupertcollins@gmail.com>
2019-08-16 22:06:15 +0100

    updated dates for next run

--- scripts/beast.sh
+++ scripts/beast.sh
@@ -4,13 +4,13 @@
 java -jar ~/Software/jmodeltest2/dist/jModelTest.jar -d pacus.haps.trimmed.nex -s 5 -S NNI -g 4 -tr 8 -f -AICc -BIC -w -o pacus.haps.trimmed.nex.out
 
 # beast run1 ~1.42 minutes/million states
-beast -beagle_auto -overwrite -seed 1408191 pacus.haps.trimmed.run1.xml
+beast -beagle_auto -overwrite -seed 1608191 pacus.haps.trimmed.run1.xml
 
 # beast run2
-beast -beagle_auto -overwrite -seed 1408192 pacus.haps.trimmed.run2.xml
+beast -beagle_auto -overwrite -seed 1608192 pacus.haps.trimmed.run2.xml
 
 # beast run3
-beast -beagle_auto -overwrite -seed 1408193 pacus.haps.trimmed.run3.xml
+beast -beagle_auto -overwrite -seed 1608193 pacus.haps.trimmed.run3.xml
 
 # check with tracer
 tracer pacus.haps.trimmed.run1.log pacus.haps.trimmed.run2.log pacus.haps.trimmed.run3.log
